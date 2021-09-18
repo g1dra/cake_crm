6403395a7631084b0f0790187e552f5f0c8138fc
g1dra <darko.vucetic7@gmail.com>
2021-09-18 23:46:37 +0200

    init

---
+++ config/Seeds/InitSeed.php
@@ -0,0 +1,37 @@
+<?php
+
+
+use Phinx\Seed\AbstractSeed;
+
+class InitSeed extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * https://book.cakephp.org/phinx/0/en/seeding.html
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'email'    => 'admin@gmail.com',
+                'password'    => 'admin',
+                'created' => date('Y-m-d H:i:s'),
+                'modified' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'email'    => 'example@gmail.com',
+                'password'    => 'example',
+                'created' => date('Y-m-d H:i:s'),
+                'modified' => date('Y-m-d H:i:s'),
+            ]
+        ];
+
+        $users = $this->table('users');
+        $users->insert($data)
+            ->saveData();
+    }
+}
